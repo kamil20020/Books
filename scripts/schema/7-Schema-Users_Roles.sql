622b276275bf94039d429a49e744858d7f349dae
kdywan_254049 <idywan_254049@e-science.pl>
2025-04-10 22:22:34 +0200

    Added left foreign keys

--- scripts/schema/7-Schema-Users_Roles.sql
+++ scripts/schema/7-Schema-Users_Roles.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS USERS_ROLES(
     user_id UUID NOT NULL,
     role_id UUID NOT NULL,
-    CONSTRAINT pk_users_roles PRIMARY KEY (user_id, role_id)
+    CONSTRAINT pk_users_roles PRIMARY KEY (user_id, role_id),
+	CONSTRAINT fk_users_roles_user FOREIGN KEY (user_id),
+	CONSTRAINT fk_users_roles_role FOREIGN KEY (role_id)
 );
