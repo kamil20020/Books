69e297239f0a5b81d7122da7f2bb1bfc089dc8fe
kdywan_254049 <idywan_254049@e-science.pl>
2025-04-10 19:06:46 +0200

    Added final publishers and authors and cleared books and atuhors

--- scripts/data/1-zPublishers.sql
+++ scripts/data/1-zPublishers.sql
@@ -1,5 +1,5 @@
 INSERT INTO PUBLISHERS
 VALUES
-       ('45e1151f-8edd-4fda-bb65-de74790c24be', 'Agencja Dramatu i Teatru'),
-       ('6e64d4ae-d5e7-426c-940a-b03e0cf89b6d', 'Agencja Wydawnicza Runa'),
-       ('b387dc7d-ecc9-4dbd-9721-4a79d4daaff9', 'Agencja Wydawniczo-Reklamowa Skarpa Warszawska');
+   ('45e1151f-8edd-4fda-bb65-de74790c24be', 'SUPERNOWA-Niezależna Oficyna Wydawnicza NOWA'),
+   ('6e64d4ae-d5e7-426c-940a-b03e0cf89b6d', 'Helion'),
+   ('b387dc7d-ecc9-4dbd-9721-4a79d4daaff9', 'Wydawnictwo naukowe PWN');
