1b24b1050f8169d257e41f122db6949c859d2032
kdywan_254049 <idywan_254049@e-science.pl>
2025-04-10 21:31:44 +0200

    Final data

--- scripts/data/1-zPublishers.sql
+++ scripts/data/1-zPublishers.sql
@@ -2,4 +2,5 @@ INSERT INTO PUBLISHERS
 VALUES
    ('45e1151f-8edd-4fda-bb65-de74790c24be', 'SUPERNOWA-Niezależna Oficyna Wydawnicza NOWA'),
    ('6e64d4ae-d5e7-426c-940a-b03e0cf89b6d', 'Helion'),
-   ('b387dc7d-ecc9-4dbd-9721-4a79d4daaff9', 'Wydawnictwo naukowe PWN');
+   ('b387dc7d-ecc9-4dbd-9721-4a79d4daaff9', 'Wydawnictwo naukowe PWN'),
+   ('899ccd6e-fe04-4ca6-878a-cdd31b9b9627', 'Mag');
--- scripts/data/2-zAuthors.sql
+++ scripts/data/2-zAuthors.sql
@@ -1,10 +1,10 @@
 INSERT INTO AUTHORS
 VALUES
-    ('430bdf0b-f8f8-4ecc-876d-ece74157f731', '45e1151f-8edd-4fda-bb65-de74790c24be', 'Andrzej', 'Sapkowski', 0),
-    ('70631d13-11b7-4633-868b-719264adf64d', null, 'Cay', 'Horstmann', 0),
-    ('7baa292e-e69d-4022-b7f4-8c23da6ac89f', null, 'Robert C.', 'Martin', 0),
-    ('8e25207a-06de-4dae-95ac-e8a1064d682f', 'b387dc7d-ecc9-4dbd-9721-4a79d4daaff9', 'Lech', 'Banachowski', 0),
-    ('fee3f271-ec75-4df1-a4ad-4c8cf8a6009e', 'b387dc7d-ecc9-4dbd-9721-4a79d4daaff9', 'Krzystof', 'Diks', 0),
-    ('e055f044-6b4d-48d5-8e99-a02435a6a5c7', 'b387dc7d-ecc9-4dbd-9721-4a79d4daaff9', 'Wojciech', 'Rytter', 0),
-    ('9be7f086-b02f-4efc-ac04-fd650a4d0d01', null, 'Daniel', 'Abraham', 0),
-    ('23042143-6657-41ac-9ac8-801a9737d16d', null, 'Ty', 'Franck', 0);
+    ('430bdf0b-f8f8-4ecc-876d-ece74157f731', '45e1151f-8edd-4fda-bb65-de74790c24be', 'Andrzej', 'Sapkowski', 4),
+    ('70631d13-11b7-4633-868b-719264adf64d', null, 'Cay', 'Horstmann', 2),
+    ('7baa292e-e69d-4022-b7f4-8c23da6ac89f', null, 'Robert C.', 'Martin', 1),
+    ('8e25207a-06de-4dae-95ac-e8a1064d682f', 'b387dc7d-ecc9-4dbd-9721-4a79d4daaff9', 'Lech', 'Banachowski', 1),
+    ('fee3f271-ec75-4df1-a4ad-4c8cf8a6009e', 'b387dc7d-ecc9-4dbd-9721-4a79d4daaff9', 'Krzystof', 'Diks', 1),
+    ('e055f044-6b4d-48d5-8e99-a02435a6a5c7', 'b387dc7d-ecc9-4dbd-9721-4a79d4daaff9', 'Wojciech', 'Rytter', 1),
+    ('9be7f086-b02f-4efc-ac04-fd650a4d0d01', null, 'Daniel', 'Abraham', 1),
+    ('23042143-6657-41ac-9ac8-801a9737d16d', null, 'Ty', 'Franck', 1);
